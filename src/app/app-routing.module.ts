80952e493edb55e68da04ca668f985c83a81e38a
Naderab <abdellaouinader@gmail.com>
2023-06-03 10:40:15 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'products',pathMatch:'full'},
+  {path:'products',component:ProductsComponent},
+  {path:'offres',component:OffresEmploiComponentComponent},
+  {path:'details/:id',component:ProductsComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductsComponent } from './products/products.component';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsComponent,
-    OffresEmploiComponentComponent
+    OffresEmploiComponentComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
